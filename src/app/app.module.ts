da893683b391037d26fb6854357ce719319de48f
Ivan <il.muniz@live.com>
2020-07-12 17:51:27 -0400

    Generate databinding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from "./success-alert/success-alert.component";
+import { DatabindingComponent } from './databinding/databinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    DatabindingComponent
   ],
   imports: [
     BrowserModule
